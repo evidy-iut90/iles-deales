adec95d93f292d31c7ff120d1ab74829f236e5e4
evidy-iut90 <144448913+evidy-iut90@users.noreply.github.com>
2023-10-26 23:58:36 +0200

    Add files via upload

---
+++ deroule.js
@@ -0,0 +1,14 @@
+function toggle1Text() {
+  var textContent1 = document.querySelector('.zone-texte1');
+  textContent1.classList.toggle('expanded');
+}
+
+function toggle2Text() {
+  var textContent2 = document.querySelector('.zone-texte2');
+  textContent2.classList.toggle('expanded');
+}
+
+function toggle3Text() {
+  var textContent3 = document.querySelector('.zone-texte3');
+  textContent3.classList.toggle('expanded');
+}
